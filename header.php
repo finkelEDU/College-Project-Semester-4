00f8e7babae5328d719808716a03a554c90fcfd4
Oscarrscso <Oscarneiland@gmail.com>
2025-04-23 18:57:41 +0100

    manually replacing files as github is wonky on my mac

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    if($_SESSION["Admin"] == false){
+        header("location:login.php");
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/common.css" />
+
+    <title>Two Guys Store</title>
+</head>
+
+<body>
+<div id="HeaderBanner">
+    <h1>TwoGuys Store</h1>
+    <nav>
+        <a href="index.php">HOME PAGE</a>
+        <a href="products.php">PRODUCTS</a>
+        <a href="login.php">LOGIN</a>
+        <a href="logout.php">LOG OUT</a>
+        <a href="signup.php">SIGN UP</a>
+    </nav>
+</div>
+<h1>Welcome <?php echo $_SESSION["Username"];?> </h1>
