f37113585cdc9d6d0cf424181361dbca15941aea
Oscarrscso <Oscarneiland@gmail.com>
2025-04-23 19:20:14 +0100

    remove duplicate files

--- nav.php
+++
@@ -1,5 +0,0 @@
-<a href="index.php">HOME PAGE</a>
-<a href="products.php">PRODUCTS</a>
-<a href="login.php">LOGIN</a>
-<a href="logout.php">LOG OUT</a>
-<a href="signup.php">SIGN UP</a>
--- shopping.php
+++
@@ -1,8 +0,0 @@
-<?php include "templates/header.php"; ?>
-
-<h1>SHOPPING PLACEHOLDER</h1>
-<p>A fully functional shopping cart should be available here. It should have functions such as deleting items or changing quantity to the shopping cart. There should also be an option to pay for the items, leading to a new webpage with a form.</p>
-        
-<p>On top of this, there will be nice design using CSS to make all website pages look much better to look at and experience.</p>
-
-<?php include "templates/footer.php"; ?>
