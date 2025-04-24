43dcd61ec4fe770d35afac04824a30cd69346533
Oscarrscso <Oscarneiland@gmail.com>
2025-04-24 18:58:53 +0100

    making the index page look

--- public/index.php
+++ public/index.php
@@ -1,26 +1,31 @@
 <?php include "templates/header_guest.php"; ?>
 <?php include "templates/nav.php"; ?>
-	<p>Welcome to the Two Guys Store! Here, you can find games, consoles and accessories to your liking!</p>
 
-	<p>Here, we have products available for the following</p>
-		<?php  
-		
-		$products = array(
-			"Playstation 5",
-			"Playstation 4",
-			"Xbox Series",
-			"Xbox One",
-			"Nintendo Switch",
-			"Nintendo Wii",
-			"Nintendo DS",
-			"PC"
-		);
-		
-		
-		for($x = 0; $x < 8; $x++){
-		  echo $products[$x];
-		  echo "<br>";
-		}?>
+<div class="index-content"> 
+    <p class="intro-text">Welcome to the Two Guys Store! Here, you can find games, consoles and accessories to your liking!</p>
 
-		<p>Are you ready to explore? You can browse the catalog, but you must be logged in to buy!</p>
+    <p>Here, we have products available for the following:</p> 
+
+    <ul class="platform-list"> 
+        <?php
+        $products = array(
+            "Playstation 5",
+            "Playstation 4",
+            "Xbox Series",
+            "Xbox One",
+            "Nintendo Switch",
+            "Nintendo Wii",
+            "Nintendo DS",
+            "PC"
+        );
+
+        foreach ($products as $product) { 
+          echo "<li>" . $product . "</li>"; 
+        }
+        ?>
+    </ul>
+
+    <p class="cta-text">Are you ready to explore? You can browse the catalog, but you must be logged in to buy!</p>
+</div>
+
 <?php include "templates/footer.php" ?>
