e19cae8857ae886bdbccd55820a1b63057ad4bb7
Fintan Kelly <B00099936@mytudublin.ie>
2025-04-27 22:04:22 +0100

    Cart.php Updated
    Updated with functions for adding and removing items. Items are referenced with a number.

--- scripts/Cart.php
+++ scripts/Cart.php
@@ -1,24 +1,59 @@
 <?php
 class Cart{
 	//ATTRIBUTES
-	public $cartItems = array(0, 0, 0, 0, 0);
+	public $cartItems = array(-1, -1, -1, -1, -1);
+	
+	public function __construct(){
+		//Not used
+	}
+	
+	public function setCartItem($arrayNumber, $itemNumber){
+		$this->cartItems[$arrayNumber] = $itemNumber;
+	}
+	
+	public function getCartItem($arrayNumber){
+		return $this->cartItems[$arrayNumber];
+	}
 	
 	public function addItem($itemNumber){
-		if($cartItems[0] == 0){
-			$cartItems[0] = $itemNumber;
+		if($this->cartItems[0] == -1){
+			$this->cartItems[0] = $itemNumber;
+		}else if($this->cartItems[1] == -1){
+			$this->cartItems[1] = $itemNumber;
+		}else if($this->cartItems[2] == -1){
+			$this->cartItems[2] = $itemNumber;
+		}else if($this->cartItems[3] == -1){
+			$this->cartItems[3] = $itemNumber;
+		}else if($this->cartItems[04] == -1){
+			$this->cartItems[4] = $itemNumber;
+		}
+	}
+	
+	public function removeItem($itemNumber){
+		if($this->cartItems[4] != -1){
+			$this->cartItems[4] = $itemNumber;
+		}else if($this->cartItems[3] != -1){
+			$this->cartItems[3] = $itemNumber;
+		}else if($this->cartItems[2] != -1){
+			$this->cartItems[2] = $itemNumber;
+		}else if($this->cartItems[1] != -1){
+			$this->cartItems[1] = $itemNumber;
+		}else if($this->cartItems[0] != -1){
+			$this->cartItems[0] = $itemNumber;
 		}
 	}
 
 
 
 	//DISPLAY DETAILS
-	public function displayMember(){
-		echo "<br>MEMBER";
-		echo "<br>ID: " . $this->getMemberID();
-		echo "<br>Username: " . $this->getMemberUsername();
-		echo "<br>Password: " . $this->getMemberPassword();
-		echo "<br>Email: " . $this->getMemberEmail();
-		echo "<br>Date of Birth: " . $this->getMemberDOB();
+	public function displayCart(){
+		echo "<br>CART ITEMS";
+		echo "<br>Item 1: " . $this->getCartItem(0);
+		echo "<br>Item 2: " . $this->getCartItem(1);
+		echo "<br>Item 3: " . $this->getCartItem(2);
+		echo "<br>Item 4: " . $this->getCartItem(3);
+		echo "<br>Item 5: " . $this->getCartItem(4);
 	}
 }
+
 ?>
