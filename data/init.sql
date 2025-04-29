e2e3c1c849a75f091e8599ac2e5639d9ce74b735
Fintan Kelly <B00099936@mytudublin.ie>
2025-04-29 18:38:44 +0100

    Changed cost of items in init.sql
    All were €499.99 prior to this.

--- data/init.sql
+++ data/init.sql
@@ -33,16 +33,16 @@ INSERT INTO Member(member_username, member_password, member_email, member_dob, m
 INSERT INTO Member(member_username, member_password, member_email, member_dob, member_type) VALUES ("fortniteGAMER33", "Iluvfortnite", "fortniteGAMER@fin.com", "2010-07-30", "Member");
 
 /*Insert the Products*/
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 5", "Newest Playstation! Buy now and join the newest games!", 499.99, "images/ps5.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 4", "Older Playstation that's still very good!", 499.99, "images/ps4.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 3", "Good for old times!", 499.99, "images/ps3.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 2", "Good for very very old times!", 499.99, "images/ps2.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 5", "Newest Playstation! Buy now and join the newest games!", 549.99, "images/ps5.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 4", "Older Playstation that's still very good!", 399.99, "images/ps4.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 3", "Good for old times!", 299.99, "images/ps3.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Playstation 2", "Good for very very old times!", 199.99, "images/ps2.png");
 INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One Series X", "Best Xbox in existence!", 499.99, "images/xbox_series_x.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One Series S", "Great Xbox that's also compact!", 499.99, "images/xbox_series_s.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One X", "Similar to Playstation 4, except an Xbox.", 499.99, "images/xbox_one_x.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One S", "Less power, but still good!", 499.99, "images/xbox_one_s.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch OLED", "Big screen with better quality!", 499.99, "images/nintendo_switch_oled.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch", "Original System to play on TV or portable!", 499.99, "images/nintendo_switch.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch Lite", "Smaller Switch that can be played on the go!", 499.99, "images/nintendo_switch_lite.png");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Wii", "Very very old Nintendo with swing controls.", 499.99, "images/nintendo_wii.jpg");
-INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Steam Deck", "Play high-level PC games on the go!", 499.99, "images/steam_deck.jpg");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One Series S", "Great Xbox that's also compact!", 399.99, "images/xbox_series_s.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One X", "Similar to Playstation 4, except an Xbox.", 299.99, "images/xbox_one_x.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Xbox One S", "Less power, but still good!", 199.99, "images/xbox_one_s.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch OLED", "Big screen with better quality!", 349.99, "images/nintendo_switch_oled.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch", "Original System to play on TV or portable!", 299.99, "images/nintendo_switch.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Switch Lite", "Smaller Switch that can be played on the go!", 249.99, "images/nintendo_switch_lite.png");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Nintendo Wii", "Very very old Nintendo with swing controls.", 99.99, "images/nintendo_wii.jpg");
+INSERT INTO Product(product_name, product_description, product_cost, product_image) VALUES ("Steam Deck", "Play high-level PC games on the go!", 639.99, "images/steam_deck.jpg");
