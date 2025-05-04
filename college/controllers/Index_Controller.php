2dba0c0c14aacc8d9943891241f0584638338f4f
Fintan Kelly <B00099936@mytudublin.ie>
2025-05-04 16:11:07 +0100

    Delete extra directory
    Added extra directory of all project files by mistake

--- college/controllers/Index_Controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-class IndexController {
-    public function index() {
-        include '../views/index_view.php';
-    }
-}
--- college/public/deleteOrder.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../models/AdminControl.php";
-$AdminControl = new AdminControl();
-	
-if(isset($_GET["id"])){
-	$AdminControl->deleteOrder();
-}
-
-header("Location: admin.php");
